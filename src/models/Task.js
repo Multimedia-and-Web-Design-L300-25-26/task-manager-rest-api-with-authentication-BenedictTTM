3f2009391df258ef8b6a606463108b8dd7c21b74
Benedict Afotey <palomakut1@gmail.com>
2026-03-03 12:23:53 +0000

    feat: Create Mongoose User and Task models with defined schemas.

--- src/models/Task.js
+++ src/models/Task.js
@@ -8,8 +8,13 @@ import mongoose from "mongoose";
 // - owner (ObjectId, ref "User", required)
 // - createdAt (default Date.now)
 
+
 const taskSchema = new mongoose.Schema({
-  
+    title: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    createdAt: { type: Date, default: Date.now },
 });
 
 const Task = mongoose.model("Task", taskSchema);
